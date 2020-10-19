fcb864e3a7b9544a53562aceda561de04081782a
KenFai <ngkenfai@gmail.com>
2020-10-19 18:39:43 +0800

    Initial Commit

---
+++ DeepLink/DeepLinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeepLinkApp.swift
+//  DeepLink
+//
+//  Created by Ginger on 19/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct DeepLinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
